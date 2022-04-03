44060d07550efb76aa0edaa20862a41a8d89a774
Isco <abdooisco24@gmail.com>
2022-04-04 00:32:31 +0200

    jQuery Lab

---
+++ jQuery/jQuery Lab/items.js
@@ -0,0 +1,37 @@
+const items = [
+  {
+    name: "Sushi ",
+    price: 120,
+    imagsrc: "images/sushi.jpg",
+  },
+  {
+    name: "Sobia Juice",
+    price: 40,
+    imagsrc: "images/sobia.jpg",
+  },
+  {
+    name: "Ramin",
+    price: 70,
+    imagsrc: "images/ramin.jpg",
+  },
+  {
+    name: "Burger",
+    price: 80,
+    imagsrc: "images/burger.jpg",
+  },
+  {
+    name: "Pizza",
+    price: 80,
+    imagsrc: "images/pizza.jpg",
+  },
+  {
+    name: "Coffee",
+    price: 20,
+    imagsrc: "images/coffee.jpg",
+  },
+  {
+    name: "Corwason",
+    price: 25,
+    imagsrc: "images/corwason.jpg",
+  },
+];
